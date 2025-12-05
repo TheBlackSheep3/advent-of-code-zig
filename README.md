e9f54c186ac0487cf26547a18435afec9b1cb02a
Niklas Dießner <40034835+TheBlackSheep3@users.noreply.github.com>
2025-12-05 18:06:37 +0100

    create zig executable
    able to read in from stdin and file and write to stdout and file

---
+++ README.md
@@ -0,0 +1,4 @@
+# advent-of-code
+This is a small project for collecting the solutions for the [Advent of
+Code](https://adventofcode.com) challenges. It relies on the usage of Zig to
+get more familiar with the language.
---
+++ src/main.zig
@@ -0,0 +1,104 @@
+const std = @import("std");
+const clap = @import("clap");
+
+const USAGE_FMT =
+    \\Usage: {s} [-i <FILE>] [-o <FILE>]
+    \\
+    \\Options:
+    \\{s}
+    \\
+;
+
+const parameter_help =
+    \\-h, --help           Display this help and exit.
+    \\-i, --input <FILE>   Path to the file to read in.
+    \\-o, --output <FILE>  Path to the file to write to.
+    \\
+;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    // First we specify what parameters our program can take.
+    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
+    const params = comptime clap.parseParamsComptime(parameter_help);
+
+    const parsers = comptime .{
+        .FILE = clap.parsers.string,
+    };
+    // Initialize our diagnostics, which can be used for reporting useful errors.
+    // This is optional. You can also pass `.{}` to `clap.parse` if you don't
+    // care about the extra information `Diagnostic` provides.
+    var res = try clap.parse(clap.Help, &params, parsers, .{
+        .allocator = arena.allocator(),
+    });
+    defer res.deinit();
+
+    // buffer for writing output
+    var stdout_buffer: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    if (res.args.help != 0)
+    {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+        defer _ = gpa.deinit();
+
+        const argv = try std.process.argsAlloc(allocator);
+        defer std.process.argsFree(allocator, argv);
+        try stdout.print(USAGE_FMT, .{std.fs.path.basename(argv[0]),parameter_help});
+        try stdout.flush();
+        return;
+    }
+
+    var read_buffer: [4096]u8 = undefined;
+    var in: std.fs.File = undefined;
+    if (res.args.input) |f| {
+        in = try std.fs.cwd().openFile(f, .{});
+    } else {
+        in = std.fs.File.stdin();
+    }
+    defer in.close();
+    var reader = in.reader(&read_buffer);
+    // if (res.args.input) |f| {
+    //     const file = try std.fs.cwd().openFile(f, .{});
+    //     defer file.close();
+    //     reader = file.reader(&read_buffer);
+    // } else {
+    //     reader = std.fs.File.stdin().reader(&read_buffer);
+    // }
+
+    var write_buffer: [4096]u8 = undefined;
+    var out: std.fs.File = undefined;
+    if (res.args.output) |f| {
+        out = try std.fs.cwd().createFile(f, .{});
+    } else {
+        out = std.fs.File.stdout();
+    }
+    defer out.close();
+    var writer = out.writer(&write_buffer);
+    // if (res.args.output) |f| {
+    //     const file = try std.fs.cwd().openFile(f, .{});
+    //     defer file.close();
+    //     writer = file.writer(&write_buffer);
+    // } else {
+    //     writer = std.fs.File.stdout().writer(&write_buffer);
+    // }
+
+    const read_interface = &reader.interface;
+    const write_interface = &writer.interface;
+    var line_no: usize = 0;
+    while (try read_interface.takeDelimiter('\n')) |line| {
+        line_no += 1;
+        try write_interface.print("Read line: '{s}'\n", .{line});
+    }
+    try write_interface.print(
+        \\============================
+        \\= Read {d: >4} lines in total =
+        \\============================
+        \\
+        , .{line_no});
+    try write_interface.flush();
+}
